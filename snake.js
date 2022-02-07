efb8a73898a2ae8e00e3e8706eab2bd5a50f1594
Karl Thomsson <thomsson_99@hotmail.com>
2022-02-07 23:43:20 +0100

    move/gameover/headcolor

--- snake.js
+++ snake.js
@@ -5,21 +5,30 @@ const gameBoardContext = gameBoard.getContext("2d");
 let dx = 10;
 let dy = 0;
 
+let changingDirection = false;
+
 let snake = [
 	{ x: 200, y: 200 },
 	{ x: 190, y: 200 },
 	{ x: 180, y: 200 },
 	{ x: 170, y: 200 },
 	{ x: 160, y: 200 },
+	{ x: 150, y: 200 },
+	{ x: 140, y: 200 },
 ];
 
 const drawSnake = () => {
 	snake.forEach((part) => {
-		gameBoardContext.fillStyle = "red";
+		gameBoardContext.fillStyle = "green";
 		gameBoardContext.strokeStyle = "black";
 		gameBoardContext.fillRect(part.x, part.y, 10, 10);
 		gameBoardContext.strokeRect(part.x, part.y, 10, 10);
 	});
+	// Paint head different color hehe
+	gameBoardContext.fillStyle = "orange";
+	gameBoardContext.strokeStyle = "black";
+	gameBoardContext.fillRect(snake[0].x, snake[0].y, 10, 10);
+	gameBoardContext.strokeRect(snake[0].x, snake[0].y, 10, 10);
 };
 
 const clearCanvas = () => {
@@ -40,12 +49,14 @@ const changeDirection = (e) => {
 	const RIGHT_KEY = 39;
 	const UP_KEY = 38;
 	const DOWN_KEY = 40;
-	let keyPressed = e.keycode;
+	let keyPressed = e.keyCode;
 
-	let down = dy === -10;
-	let up = dy === 10;
-	let right = dx === 10;
-	let left = dx === -10;
+	let down  =  10 === dy;
+	let up    = -10 === dy;
+	let right =  10 === dx;
+	let left  = -10 === dx;
+	if(changingDirection) return;
+	changingDirection = true;
 
 	if (keyPressed === LEFT_KEY && !right) {
 		dy = 0;
@@ -56,22 +67,61 @@ const changeDirection = (e) => {
 		dx = 10;
 	}
 	if (keyPressed === UP_KEY && !down) {
-		dy = 10;
+		dy = -10;
 		dx = 0;
 	}
 	if (keyPressed === DOWN_KEY && !up) {
-		dy = -10;
+		dy = 10;
 		dx = 0;
 	}
 };
 
+const gameOver = () => {
+	let head = {x:snake[0].x,y:snake[0].y}
+
+	for(let i=6; i < snake.length; i++) {
+		if (snake[i].x === head.x && snake[i].y === head.y) return true;
+	}
+	// Wall checks
+	if (head.x < 0 ) {
+		// Hit left wall 
+		return true;
+	}
+	if (head.x > gameBoard.width - 10) {
+		// Hit right wall
+		return true;
+	}
+	if (head.y < 0) {
+		// Hit top wall
+		return true;
+	}
+	if (head.y > gameBoard.height - 10) {
+		// Hit bottom wall
+		return true;
+	}
+
+	return false;
+}
+
+const drawGameOver = () => {
+	gameBoardContext.strokeStyle = "red";
+	gameBoardContext.lineWidth = 7;
+	gameBoardContext.strokeRect(0, 0, gameBoard.width, gameBoard.height);
+}
+
 function main() {
 	setInterval(() => {
+		changingDirection = false;
+		if (gameOver()){
+			drawGameOver();
+			return;
+		}
 		clearCanvas();
 		drawSnake();
 		slither();
 	}, 100);
 }
 
+document.addEventListener("keydown", changeDirection);
 main();
-document.addEventListener("keydown", change_direction);
+
